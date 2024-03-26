5ba7411aee8a48bc2056aed357b5573083d6658a
AndreiKuzmenko <eccehomo28@gmail.com>
2024-03-26 19:05:56 +0200

    26022024

--- src/main/java/manager/HelperBase.java
+++ src/main/java/manager/HelperBase.java
@@ -1,12 +1,23 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HelperBase {
     WebDriver wd;
     public HelperBase(WebDriver wd) {
         this.wd = wd;
     }
-
+    public void type(By locator, String text){
+        WebElement element = wd.findElement(locator);
+        // element.click();
+        element.clear();
+        element.sendKeys(text);
+    }
+    public boolean isElementPresent(By locator)
+    {
+        return wd.findElements(locator).size() > 0;
+    }
 
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,7 +1,16 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
 
 @Test
-    public void LoginPositive(){}
+    public void LoginPositive(){
+
+    String email = "qwe777@gmail.com", password = "123@Qwe123";
+    app.getUser().openLoginForm();
+    app.getUser().fillLoginForm(email, password);
+    app.getUser().submitLogin();
+    Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//button")));
+}
 }
